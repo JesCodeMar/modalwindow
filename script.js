cf3f501bf7a76a21215e763f800c4ad211250fbb
Jesreel Marbaniang <mophilo17@gmail.com>
2023-11-23 18:59:02 +0530

    ESC key functionality added

--- script.js
+++ script.js
@@ -3,12 +3,28 @@ const modal=document.querySelector('.modal');
 const overlay=document.querySelector('.overlay');
 const closemodalbtn=document.querySelector('.close-modal');
 const openmodabtn=document.querySelectorAll('.show-modal');
-for(let i=0;i<openmodabtn.length;i++)
-    openmodabtn[i].addEventListener('click',function(){
-        modal.classList.remove('hide');
-        overlay.classList.remove('hide')
-    })
-closemodalbtn.addEventListener('click',function(){
+
+const open=function(){
+    modal.classList.remove('hide');
+    overlay.classList.remove('hide');
+}
+
+const close=function(){
     modal.classList.add('hide');
     overlay.classList.add('hide');
-})
+}
+
+for(let i=0;i<openmodabtn.length;i++)
+    openmodabtn[i].addEventListener('click',open);
+
+closemodalbtn.addEventListener('click',close);
+
+overlay.addEventListener('click',close);
+
+document.addEventListener('keydown',function(event){
+    if(event.key==='Escape'){
+        if(!modal.classList.contains('hide')){
+            close();
+        }
+    }
+});
