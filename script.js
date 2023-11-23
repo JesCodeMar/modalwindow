733a3441d845de42121217089b246e7dfef267b8
Jesreel Marbaniang <mophilo17@gmail.com>
2023-11-23 13:53:54 +0530

    js script added

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 'use strict';
+const modal=document.querySelector('.modal');
+const overlay=document.querySelector('.overlay');
+const closemodalbtn=document.querySelector('.close-modal');
+const openmodabtn=document.querySelectorAll('.show-modal');
+for(let i=0;i<openmodabtn.length;i++)
+    openmodabtn[i].addEventListener('click',function(){
+        modal.classList.remove('hide');
+        overlay.classList.remove('hide')
+    })
+closemodalbtn.addEventListener('click',function(){
+    modal.classList.add('hide');
+    overlay.classList.add('hide');
+})
